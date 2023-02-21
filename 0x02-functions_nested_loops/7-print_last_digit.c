ed4cb1973ffc5b1a538f3fd2ed93a2475b6be23e
J-Zeal <jeremiahamodu@gmail.com>
2023-02-21 09:55:10 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,11 @@
  *
  * Return: Value of last digit
  */
+
 int print_last_digit(int n)
 {
-	if ((n % 10) < 0)
-		n *= -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+        if ((n % 10) < 0)
+                n *= -1;
+        _putchar((n % 10) + '0');
+        return (n % 10);
 }
