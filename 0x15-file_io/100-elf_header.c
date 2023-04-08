5a3ad8c9e7ff3aa53525ee694f9d92759ae09101
J-Zeal <jeremiahamodu@gmail.com>
2023-04-08 03:04:24 -0700

    Jeremiah on input and output files

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * main - Copies the content of a file to another file.
+ * @ac: Argument count
+ * @av: argument values
+ * Return: 0 on succes, -1 on error.
+ */
+int main(int ac, char *av[])
+{
+	return (1);
+	return (0);
+}
