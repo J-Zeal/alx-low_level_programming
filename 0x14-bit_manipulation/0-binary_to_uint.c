226f83024114848beffb2ca73efc1ed393e99ffe
J-Zeal <jeremiahamodu@gmail.com>
2023-04-08 05:05:29 -0700

    Update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,62 +1,37 @@
 #include "main.h"
 
 /**
- * powX - raises b to p
- * @b: base
- * @p: power
- * Return: b the power of p
- */
-int powX(int b, int p)
-{
-	int prod = 1;
-
-	while (p > 0)
-	{
-		prod *= b;
-		p--;
-	}
-	return (prod);
-}
-
-/**
- * _len - length of a string
- * @s:string
- * Return: lenght of s
- */
-int _len(const char *s)
-{
-	int len = 0;
-
-	while (*s)
-	{
-		len++;
-		s++;
-	}
-	return (len);
-}
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: input string
- * Return: unsigned decimal conversion, 0 if input is NULL
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int power;
-	int num = 0;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
-	power = _len(b) - 1;
-	while (*b)
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
+		}
 
-		if (*b == '1')
-			num += powX(2, power);
-		b++;
-		power--;
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (num);
-}
 
+	return (ui);
+}
